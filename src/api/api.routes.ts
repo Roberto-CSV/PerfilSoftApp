1e8643ab14f2c58f0d976e91cdf3e4aed5db1d05
Roberto <robertoslas0112@gmail.com>
2022-11-27 23:08:17 -0500

    respuesta-seccion-cuestionario

--- src/api/api.routes.ts
+++ src/api/api.routes.ts
@@ -23,7 +23,7 @@ export const apiRoutes = [
     module: UsuariosModule,
   },
   {
-    path: 'usuario',
+    path: 'respuesta-seccion-cuestionario',
     module: RespuestaSeccionCuestionarioModule,
   },
   {
